124412810583b9aa0f68ef3ba6292193c938e442
CakeFactorie <162422596+thatcupcakefactorie@users.noreply.github.com>
2025-06-26 06:41:23 -0400

    V1.1 complete difficulty and player movement overhaul

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 function startGame (difficulty: number) {
     Player = game.createSprite(2, 4)
+    Player.set(LedSpriteProperty.Brightness, 127)
     Enemy = [game.createSprite(randint(0, 4), 0)]
     gameActive = true
     LoopCount = 0
     while (true) {
-        basic.pause(2000 / ((LoopCount / 50 + 1) * (Difficulty + 1) / 3))
+        basic.pause(2000 / ((LoopCount / 35 + 1) * (Difficulty + 1) / 3))
         if (!(GameOver)) {
             if (Enemy.length > 0) {
                 for (let index = 0; index <= Enemy.length - 1; index++) {
@@ -37,7 +38,7 @@ function startGame (difficulty: number) {
             } else {
                 Enemy.push(game.createSprite(randint(0, 4), 0))
             }
-            LoopCount += 3
+            LoopCount += 1
         }
     }
 }
@@ -46,6 +47,9 @@ input.onButtonPressed(Button.A, function () {
         if (!(GameOver)) {
             music.play(music.createSoundExpression(WaveShape.Sine, 1725, 1725, 204, 155, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
             Player.move(-1)
+            if (Player.get(LedSpriteProperty.X) <= 0) {
+                Player.set(LedSpriteProperty.X, 4)
+            }
         }
     }
     if (DifficultyMenu) {
@@ -56,15 +60,16 @@ input.onButtonPressed(Button.A, function () {
 function gameOverSequence () {
     music.stopAllSounds()
     if (Enemy.length > 0) {
-        for (let index = 0; index <= Enemy.length - 2; index++) {
+        for (let index = 0; index <= Enemy.length - 0; index++) {
             Enemy.pop().delete()
         }
     }
     Player.delete()
     music.play(music.createSoundExpression(WaveShape.Sawtooth, 1506, 0, 255, 252, 3000, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     basic.clearScreen()
+    music.play(music.createSoundExpression(WaveShape.Square, 408, 408, 98, 0, 1000, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
     basic.showString("Game Over!")
-    music.play(music.createSoundExpression(WaveShape.Square, 408, 1, 98, 0, 2000, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
+    basic.showString("Score..." + convertToText(LoopCount))
     basic.pause(2000)
     control.reset()
 }
@@ -73,6 +78,9 @@ input.onButtonPressed(Button.B, function () {
         if (!(GameOver)) {
             music.play(music.createSoundExpression(WaveShape.Sine, 1725, 1725, 204, 155, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
             Player.move(1)
+            if (Player.get(LedSpriteProperty.X) >= 4) {
+                Player.set(LedSpriteProperty.X, 0)
+            }
         }
     }
     if (DifficultyMenu) {
